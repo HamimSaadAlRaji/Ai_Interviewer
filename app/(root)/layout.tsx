e881a996155023cd3df7f246788735fa2d9be15a
HamimSaadAlRaji <hamimsaad.raji@gmail.com>
2025-05-06 16:01:57 +0600

    Homepage Boilerplate

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import React, { ReactNode } from "react";
 
-const Rootlayout = () => {
-  return <div>Home Page</div>;
+const Rootlayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="root-layout">
+      <nav>
+        <Link href="/" className="flex items-center gap-2 ">
+          <Image
+            className="logo"
+            src="/logo.svg"
+            alt="Logo"
+            width={38}
+            height={32}
+          />
+          <h2 className="text-primary-100">My App</h2>
+        </Link>
+      </nav>
+      {children}
+    </div>
+  );
 };
 
 export default Rootlayout;
